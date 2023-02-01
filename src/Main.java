edabb28d1c7c61394d56713f62aaf7aebf0dbb36
user <gulira.abdukerimkyzy.jk@gmail.com>
2023-02-01 20:26:00 +0600

    addTask1

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        Person person=new Person();
+        person.name="Milana";
+        person.surname= "Arkadi";
+        person.age= 25;
+        person.placeOfBirth=" Russia Moscow";
+        person.residentialAddress="Kyrgyzstan, Bishkek";
+        person.phoneNumber= 779444510;
+        System.out.print("Name: "+person.name+"\n"+"Surname: "+person.surname+"\n"+"Age: "
+                +person.age+"\n"+"Place of birth: "+person.placeOfBirth+
+                "\n"+"Residential address: "+person.residentialAddress+"\n"
+                +"Phone number: +996"+person.phoneNumber+"\n  Milana 18 жашынан бери ");
+        person.canDriveACar();
+        System.out.println("/////////////////////////////////");
+        Person person2=new Person();
+        person2.name="Lana";
+        person2.surname= "Ahmadi";
+        person2.age= 24;
+        person2.placeOfBirth=" Kazakhstan Astana";
+        person2.residentialAddress="Kyrgyzstan, Osh ";
+        person2.phoneNumber= 779040500;
+
+
+        System.out.print("Name: "+person2.name+"\n"+"Surname: "+person2.surname+"\n"+"Age: "
+                +person2.age+"\n"+"Place of birth: "+person2.placeOfBirth+
+                "\n"+"Residential address: "+person2.residentialAddress+"\n"
+                +"Phone number: +996"+person2.phoneNumber+"\n  Lana 18 жашынан баштап  ");
+        person2.canWriteCodeInJava();
+
     }
+
+
 }
--- src/Person.java
+++ src/Person.java
@@ -1,2 +1,24 @@
 public class Person {
+    String name;
+    String surname;
+    int age;
+    String placeOfBirth;
+    String residentialAddress;
+    int phoneNumber;
+
+
+    //What can a person do?
+   public  void canWriteCodeInJava(){
+       System.out.println("Жавада код жаза алат");
+   }
+   public  void canDriveACar(){
+       System.out.println("Машина айдай алат");
+   }
+            //Сол жактагылардан бирөөсун тандап класс
+    //түзүңүз.
+    //Полелерин кошуңуз.
+    //Main методтон ушул класстын 3 объектисин
+    //түзүңүз.
+    //3 объектти маани берип толтуруңуз.
+    //Баардык маанилерин консолго чыгарыңыз
 }
